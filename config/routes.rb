60990fa513be840553cda9c67fb84d038d64bffb
Mark Wang <zhengkaw@gmail.com>
2014-05-26 17:21:54 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   root 'static_pages#home'
+  get '/sessions', to: 'sessions#new'
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
+
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
